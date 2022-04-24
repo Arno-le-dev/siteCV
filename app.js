20cf094efdb375e9ca6e48d62917edd4a7174ab8
Arno Bruno <61425813+Arno-le-dev@users.noreply.github.com>
2022-04-25 01:44:00 +0200

    page Projets DONE

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const items = document.querySelectorAll('div.projet');
 const nbSlide = items.length;
-const suivant = document.querySelector('.right')
-const precedent = document.querySelector('.left')
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
 let count =0; 
+
+const item_slideshow = document.querySelectorAll('div.rond_slideshow')
 
 function slideSuivante() {
     items[count].classList.remove('active');
-
+    item_slideshow[count].classList.remove('active');
     if (count < nbSlide - 1){
         count++;
     }else {
         count=0;
     }
     items[count].classList.add('active'); 
+    item_slideshow[count].classList.add('active');
 }
 suivant.addEventListener('click', slideSuivante)
 
 function slidePrecedente() {
     items[count].classList.remove('active');
+    item_slideshow[count].classList.remove('active');
 
     if (count > 0){
         count--;
@@ -25,7 +29,7 @@ function slidePrecedente() {
         count=nbSlide - 1;
     }
     items[count].classList.add('active'); 
-    
+    item_slideshow[count].classList.add('active');
 }
 
 precedent.addEventListener('click', slidePrecedente);
